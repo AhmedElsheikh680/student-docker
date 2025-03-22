b81fc3e9e79a7296e8f6cde7c341a97aa044fc93
Ahmed Elsheikh <ahmedelsheikh680@gmail.com>
2025-03-21 22:45:45 -0700

    docker compose(up & down)

--- src/main/java/com/student/controller/StudentController.java
+++ src/main/java/com/student/controller/StudentController.java
@@ -9,9 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1")
 public class StudentController {
 
-    @GetMapping("/studnet")
+    @GetMapping("/student")
     public String studnet() {
-        return "Ahmed Mohamed Elsheikh";
+        return "Ahmed Mohamed Elsheikh!!!";
     }
 
     @GetMapping("/hello")
