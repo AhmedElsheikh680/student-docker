b8d8d8d08aac3d2c054c58a436c5776c2ddcecdc
Ahmed Elsheikh <ahmedelsheikh680@gmail.com>
2025-03-22 03:54:37 -0700

    environment variables

--- src/main/java/com/student/controller/StudentController.java
+++ src/main/java/com/student/controller/StudentController.java
@@ -1,5 +1,6 @@
 package com.student.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1")
 public class StudentController {
 
+    @Value("${name}")
+    private String name;
+    @Value("${age}")
+    private String age;
+
     @GetMapping("/student")
     public String studnet() {
-        return "Ahmed Mohamed Elsheikh!!!";
+        return "My Name Is "+ name + " And Age Is "+ age;
     }
 
     @GetMapping("/hello")
